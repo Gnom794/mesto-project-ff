17f046713f7a3df71f5cebf6831861eccd24f913
Bystrov Egor <bystrovegorka@mail.ru>
2025-06-04 21:11:41 +0500

    Fix bags

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,3 @@
-// @todo: Темплейт карточки
-
 // @todo: DOM узлы
 
 // @todo: Функция создания карточки
@@ -8,32 +6,32 @@
 
 // @todo: Вывести карточки на страницу
 
-function createCard(card, handDelete) {
+function deleteCard(cardElement) {
+    cardElement.remove();
+}
+
+function createCard(card, handleDelete) {
     const cardTemplate = document.getElementById('card-template');
     const cardElement = cardTemplate.content.querySelector('.card').cloneNode(true);
 
     const cardImage = cardElement.querySelector('.card__image');
     const cardTitle = cardElement.querySelector('.card__title');
     const deleteButton = cardElement.querySelector('.card__delete-button');
-    const likeButton = cardElement.querySelector('.card__like-button');
 
     cardImage.src = card.link;
     cardImage.alt = card.name;
     cardTitle.textContent = card.name
 
     deleteButton.addEventListener('click', () => {
-        handDelete(cardElement);
+        handleDelete(cardElement);
     });
 
     return cardElement;
 }
 
-const placesList = document.querySelector('.places__list');
+const cardsContainer = document.querySelector('.places__list');
 
 initialCards.forEach((card) => {
-    const cardElement = createCard(card, (element) => {
-        element.remove()
+    const cardElement = createCard(card, deleteCard);
+        cardsContainer.append(cardElement);
     });
-
-    placesList.appendChild(cardElement);
-});
